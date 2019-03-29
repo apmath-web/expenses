23a2d292da66b2a5ad86b71e5a21ae47db036744
TimurSirkin <timur_sirkin@mail.ru>
2019-03-29 20:54:17 +0300

    -Создал структуру валидации и методы добавления и генерирования сообщения

--- Application/Validation/Validation.go
+++ Application/Validation/Validation.go
@@ -1,4 +1,23 @@
 package Validation
 
+import "github.com/apmath-web/expenses/Domain"
+
 type Validation struct {
+	message  string
+	messages []Domain.MessageInterface
+}
+
+func (v *Validation) AddMessage(message Domain.MessageInterface) {
+	v.messages = append(v.messages, message)
+}
+
+func GenMessage(field, text string) *Message {
+	m := new(Message)
+	m.field = field
+	m.text = text
+	return m
+}
+
+type Message struct {
+	text, field string
 }
