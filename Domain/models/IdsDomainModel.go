134e5af099775eb7fe01cdef71024c56f6e92e6e
TimurSirkin <timur_sirkin@mail.ru>
2019-04-11 22:54:27 +0300

    Create struct and implement interface

--- Domain/models/IdsDomainModel.go
+++ Domain/models/IdsDomainModel.go
@@ -1,1 +1,14 @@
 package models
+
+type IdsDomainModel struct {
+	ClientId           int
+	CoborrowersIdSlice []int
+}
+
+func (i *IdsDomainModel) GetClientId() int {
+	return i.ClientId
+}
+
+func (i *IdsDomainModel) GetCoborrowersIdSlice() []int {
+	return i.CoborrowersIdSlice
+}
