64517fadc154318d2175e8733f59efb6e1c00398
morozzArt <morozz9846@gmail.com>
2019-04-20 13:04:18 +0300

    fix name conflict

--- Infrastructure/Mapper/PersonMapper.go
+++ Infrastructure/Mapper/PersonMapper.go
@@ -2,12 +2,12 @@ package Mapper
 
 import (
 	"github.com/apmath-web/expenses/Domain"
-	"github.com/apmath-web/expenses/Domain/Models"
+	"github.com/apmath-web/expenses/Domain/models"
 	"github.com/apmath-web/expenses/Infrastructure/applicationModels"
 )
 
 func PersonApplicationMapper (am applicationModels.PersonApplicationModel) Domain.PersonDomainModelInterface {
-	DomainModel := new(Models.PersonDomainModel)
+	DomainModel := new(models.PersonDomainModel)
 	DomainModel.FirstName = am.FirstName
 	DomainModel.LastName = am.LastName
 	DomainModel.SumWage = am.GetSumWage()
