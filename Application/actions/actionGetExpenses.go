30a77b9ac184a4f44b69c2257eb681ecfa055d7a
TimurSirkin <timur_sirkin@mail.ru>
2019-05-17 22:49:33 +0300

    Getting amount

--- Application/actions/actionGetExpenses.go
+++ Application/actions/actionGetExpenses.go
@@ -1,9 +1,27 @@
 package actions
 
 import (
+	"github.com/apmath-web/expenses/Domain/models"
+	"github.com/apmath-web/expenses/Domain/services"
+	"github.com/apmath-web/expenses/Infrastructure"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func GetExpenses(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		//do something
+		return
+	}
 
+	tempSlice := []int{2, 3, 4}
+	dm := models.GenIds(id, tempSlice)
+	service := services.CalculationService{}
+
+	clientFetchService := Infrastructure.GenClientFetchService()
+	service.GenCalculationService(clientFetchService)
+	ei, err := service.Calculate(dm)
+
+	amount := ei.GetAmount()
 }
