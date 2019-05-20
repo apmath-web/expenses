5ff5c9527855ccd672505e589eaeaacfbe97f746
TimurSirkin <timur_sirkin@mail.ru>
2019-05-21 00:20:25 +0300

    Create GetExpenses function

--- Application/actions/actionGetExpenses.go
+++ Application/actions/actionGetExpenses.go
@@ -1,1 +1,55 @@
 package actions
+
+import (
+	"encoding/json"
+	"github.com/apmath-web/expenses/Application/Validation"
+	"github.com/apmath-web/expenses/Application/viewModels"
+	"github.com/apmath-web/expenses/Domain/models"
+	"github.com/apmath-web/expenses/Domain/services"
+	"github.com/apmath-web/expenses/Infrastructure"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetExpenses(c *gin.Context) {
+
+	vm := viewModels.IdsViewModel{}
+
+	if err := c.BindJSON(&vm); err != nil {
+		validator := Validation.GenValidation()
+		validator.SetMessage("validation error")
+		validator.AddMessage(Validation.GenMessage("clientId", err.Error()))
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
+
+	if !vm.Validate() {
+		validator := vm.GetValidation()
+		validator.SetMessage("validation error")
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
+
+	dm := models.GenIds(vm.GetClienId(), vm.GetCoborrowersIdSlice())
+	service := services.CalculationService{}
+
+	clientFetchService := Infrastructure.GenClientFetchService()
+	service.GenCalculationService(clientFetchService)
+	ei, err := service.Calculate(dm)
+
+	if err != nil {
+		validator := vm.GetValidation()
+		validator.SetMessage("validation error")
+		validator.AddMessage(Validation.GenMessage("clientId", err.Error()))
+		str, _ := json.Marshal(validator)
+		c.String(http.StatusBadRequest, string(str))
+		return
+	}
+
+	evm := new(viewModels.ExpensesViewModel)
+	evm.Hydrate(ei)
+
+	c.JSON(http.StatusOK, evm)
+}
