b377a3dc4690353df29ff289d55d4979ce77be46
TimurSirkin <timur_sirkin@mail.ru>
2019-05-20 23:07:12 +0300

    Change params of calculationService

--- Domain/services/calulationService.go
+++ Domain/services/calulationService.go
@@ -9,16 +9,16 @@ type CalculationService struct {
 	clientFetcher Domain.ClientFetchInterface
 }
 
-func (cs *CalculationService) Calculate(ids models.IdsDomainModel) (Domain.ExpensesInterface, error) {
+func (cs *CalculationService) Calculate(ids Domain.IdsDomainModelInterface) (Domain.ExpensesInterface, error) {
 	var persons []Domain.PersonDomainModelInterface
 
-	var pdm, err = cs.clientFetcher.Fetch(ids.ClientId)
+	var pdm, err = cs.clientFetcher.Fetch(ids.GetClientId())
 	if err != nil {
 		return nil, err
 	}
 	persons = append(persons, pdm)
 
-	for _, value := range ids.CoborrowersIdSlice {
+	for _, value := range ids.GetCoborrowersIdSlice() {
 		var pdm, err = cs.clientFetcher.Fetch(value)
 		if err != nil {
 			return nil, err
