b67c58aec84997dc16c0db339d52a09bfe345d83
morozzArt <morozz9846@gmail.com>
2019-05-24 00:26:33 +0300

    add unit test

--- Domain/services/calculate_test.go
+++ Domain/services/calculate_test.go
@@ -1,2 +1,60 @@
 package services
 
+import (
+	"github.com/apmath-web/expenses/Domain"
+	"github.com/apmath-web/expenses/Domain/models"
+	"testing"
+)
+
+func TestCalculate(t *testing.T) {
+	var calc = CalculateService{}
+	var maxValue float64
+
+	persons := []models.PersonDomainModel{
+		models.PersonDomainModel {
+			FirstName: "Ivan",
+			LastName:  "Ivanov",
+			SumWage:   5000,
+		},
+		models.PersonDomainModel {
+			FirstName: "Petr",
+			LastName: "Petrov",
+			SumWage: 2000,
+		},
+		models.PersonDomainModel {
+			FirstName: "Artem",
+			LastName: "Artemov",
+			SumWage: 6000,
+		},
+		models.PersonDomainModel {
+			FirstName: "Timur",
+			LastName: "Timurov",
+			SumWage: 7000,
+		},
+		models.PersonDomainModel {
+			FirstName: "Pasha",
+			LastName: "Pashov",
+			SumWage: 3000,
+		},
+	}
+
+	personInf := make([]Domain.PersonDomainModelInterface, len(persons))
+
+	for i, v := range persons{
+		personInf[i] = &v
+	}
+
+	_ = calc.Calculate(personInf)
+
+	if maxValue != 2300 {
+		t.Error("Incorrect answer for simple case")
+	}
+
+	persons[0].SumWage = -1
+
+	err := calc.Calculate(personInf)
+
+	if err != nil  {
+		t.Error("Negative value")
+	}
+}
