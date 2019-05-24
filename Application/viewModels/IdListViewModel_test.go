4542565496a34fe49874ff2105351925e102e269
TimurSirkin <timur_sirkin@mail.ru>
2019-05-24 03:52:42 +0300

    Add "OK" case and "Negative Client's ID" case

--- Application/viewModels/IdListViewModel_test.go
+++ Application/viewModels/IdListViewModel_test.go
@@ -1,1 +1,37 @@
 package viewModels
+
+import (
+	"github.com/apmath-web/expenses/Application/Validation"
+	"testing"
+)
+
+func TestIdsViewModel_Validate(t *testing.T) {
+	vm := IdsViewModel{}
+
+	//OK case
+	{
+		vm.ClientId = 1
+		vm.CoborrowersIdSlice = []int{2, 3, 4, 5}
+		if !vm.Validate() {
+			t.Error("Expected true, got", vm.Validate())
+		}
+	}
+
+	//Client's Id negative case
+	{
+		vm.ClientId = -1
+		vm.CoborrowersIdSlice = []int{2, 3, 4, 5}
+		vm.Validate()
+
+		trueValidation := Validation.Validation{}
+		trueValidation.AddMessage(Validation.GenMessage("clienId", "Is negative"))
+		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
+
+		resultValidation := vm.GetValidation()
+		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
+
+		if resultMessage != trueMessage {
+			t.Error("Expected", trueMessage, "got", resultMessage)
+		}
+	}
+}
