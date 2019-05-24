434b56001efc2828021653823500f817cefacd8a
TimurSirkin <timur_sirkin@mail.ru>
2019-05-24 20:03:15 +0300

    Change test structure

--- Application/viewModels/IdListViewModel_test.go
+++ Application/viewModels/IdListViewModel_test.go
@@ -1,240 +1,158 @@
 package viewModels
 
 import (
-	"github.com/apmath-web/expenses/Application/Validation"
 	"testing"
 )
 
 func TestIdsViewModel_Validate(t *testing.T) {
-	vm := IdsViewModel{}
 
-	//OK case
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{2, 3, 4, 5}
-		if !vm.Validate() {
-			t.Error("Expected true, got", vm.Validate())
-		}
-	}
-	{
-		vm.ClientId = 2
-		vm.CoborrowersIdSlice = []int{1}
-		if !vm.Validate() {
-			t.Error("Expected true, got", vm.Validate())
-		}
-	}
-	{
-		vm.ClientId = 10
-		vm.CoborrowersIdSlice = []int{23, 31, 4, 15}
-		if !vm.Validate() {
-			t.Error("Expected true, got", vm.Validate())
-		}
-	}
-	{
-		vm.ClientId = 100
-		vm.CoborrowersIdSlice = []int{2, 3, 4, 5, 6, 10, 33}
-		if !vm.Validate() {
-			t.Error("Expected true, got", vm.Validate())
-		}
+	type TestData struct {
+		clientId       int
+		coborrowersIds []int
+		field          string
+		text           string
+		isErrorCase    bool
 	}
 
-	//Client's Id negative case
+	dataSlice := []TestData{}
+
+	//Ok cases
 	{
-		vm.ClientId = -1
-		vm.CoborrowersIdSlice = []int{2, 3, 4, 5}
+		clientId := 1
+		coborrowersIds := []int{2}
+		data := TestData{clientId, coborrowersIds, "",
+			"", false}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 2
+		coborrowersIds := []int{1, 3, 4, 5}
+		data := TestData{clientId, coborrowersIds, "",
+			"", false}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 20
+		coborrowersIds := []int{2, 3, 4, 5, 412, 43}
+		data := TestData{clientId, coborrowersIds, "",
+			"", false}
+		dataSlice = append(dataSlice, data)
+	}
+
+	//Client ID negative cases
+	{
+		clientId := -1
+		coborrowersIds := []int{2}
+		data := TestData{clientId, coborrowersIds, "clienId",
+			"Is negative", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := -2
+		coborrowersIds := []int{1, 3, 4, 5}
+		data := TestData{clientId, coborrowersIds, "clienId",
+			"Is negative", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := -20
+		coborrowersIds := []int{2, 3, 4, 5, 412, 43}
+		data := TestData{clientId, coborrowersIds, "clienId",
+			"Is negative", true}
+		dataSlice = append(dataSlice, data)
+	}
+
+	//Coborrower's ID negative cases
+	{
+		clientId := 1
+		coborrowersIds := []int{-2, 90}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Is negative", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 2
+		coborrowersIds := []int{1, 3, 4, -5}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Is negative", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 20
+		coborrowersIds := []int{2, 3, -4, 5, 412, 43}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Is negative", true}
+		dataSlice = append(dataSlice, data)
+	}
+
+	//Coborrower's ID's are eaqual cases
+	{
+		clientId := 1
+		coborrowersIds := []int{2, 3, 90, 3}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Some coborrower's IDs are equal to each other", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 2
+		coborrowersIds := []int{1, 3, 3, 5}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Some coborrower's IDs are equal to each other", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 20
+		coborrowersIds := []int{2, 3, 4, 5, 412, 2}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Some coborrower's IDs are equal to each other", true}
+		dataSlice = append(dataSlice, data)
+	}
+
+	//Coborrower's ID is eaqual to client's ID
+	{
+		clientId := 1
+		coborrowersIds := []int{1, 2, 90, 3}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Coborrower's ID can't be equal to Client's ID", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 2
+		coborrowersIds := []int{1, 3, 2, 5}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Coborrower's ID can't be equal to Client's ID", true}
+		dataSlice = append(dataSlice, data)
+	}
+	{
+		clientId := 20
+		coborrowersIds := []int{2, 3, 4, 5, 412, 20}
+		data := TestData{clientId, coborrowersIds, "coBorrowers",
+			"Coborrower's ID can't be equal to Client's ID", true}
+		dataSlice = append(dataSlice, data)
+	}
+
+	for _, currentData := range dataSlice {
+
+		vm := IdsViewModel{}
+		vm.ClientId = currentData.clientId
+		vm.CoborrowersIdSlice = currentData.coborrowersIds
 		vm.Validate()
 
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("clienId", "Is negative"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
+		if currentData.isErrorCase {
+			validation := vm.GetValidation()
+			message := validation.GetMessages()[0]
+			field := message.GetField()
+			text := message.GetText()
 
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = -22
-		vm.CoborrowersIdSlice = []int{2, 3, 4, 5}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("clienId", "Is negative"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = -123
-		vm.CoborrowersIdSlice = []int{1, 2, 3}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("clienId", "Is negative"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-
-	//Coborrower's Id negative case
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{2, -3, 4, 5}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Is negative"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{21, 13, 40, 50, -3, 22}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Is negative"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{21, 13, 40, 50, 2, 22, -40}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Is negative"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-
-	//Coborrower's Id is equal to clients's Id
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{1, 2, 4, 5}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Coborrower's ID can't be equal to Client's ID"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{13, 2, 4, 5, 6, 1, 3, 5}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Coborrower's ID can't be equal to Client's ID"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{13, 2, 4, 5, 6, 10, 3, 5, 1}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Coborrower's ID can't be equal to Client's ID"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-
-	//Coborrower's Id's are equal
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{2, 2, 4, 5}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Some coborrower's IDs are equal to each other"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = 1
-		vm.CoborrowersIdSlice = []int{1, 2, 4, 5, 10, 11, 12, 10}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Some coborrower's IDs are equal to each other"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
-		}
-	}
-	{
-		vm.ClientId = 111
-		vm.CoborrowersIdSlice = []int{1, 2, 4, 1, 10, 1, 12, 10}
-		vm.Validate()
-
-		trueValidation := Validation.Validation{}
-		trueValidation.AddMessage(Validation.GenMessage("coBorrowers", "Some coborrower's IDs are equal to each other"))
-		_, trueMessage := trueValidation.GetMessages()[0].MarshalJSON()
-
-		resultValidation := vm.GetValidation()
-		_, resultMessage := resultValidation.GetMessages()[0].MarshalJSON()
-
-		if resultMessage != trueMessage {
-			t.Error("Expected", trueMessage, "got", resultMessage)
+			if text != currentData.text || field != currentData.field {
+				t.Error("Expected", currentData.text, "in", currentData.field,
+					"got", text, "in", field)
+			}
+		} else {
+			if !vm.Validate() {
+				t.Error("Expected true case",
+					"got false case")
+			}
 		}
 	}
 }
